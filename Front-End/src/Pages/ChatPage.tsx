109c0730ae09d941c0dd690ad1c7ccd5c089216d
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-09-02 00:45:42 +0530

    Added Chatpage & LogIn/LogOut

--- Front-End/src/Pages/ChatPage.tsx
+++ Front-End/src/Pages/ChatPage.tsx
@@ -1,10 +1,15 @@
 import * as css from "../Styles/ChatPageStyles";
-import { useContext, useState } from "react";
+import "regenerator-runtime/runtime";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { useContext, useState, useEffect } from "react";
 import {
   Box,
   Text,
   Image,
   Center,
+  useToast,
   Input,
   InputGroup,
   InputLeftElement,
@@ -12,14 +17,75 @@ import {
 } from "@chakra-ui/react";
 
 import { PiDotsNineBold as ThreeDots } from "react-icons/pi";
+import { HiMicrophone as MicIcon } from "react-icons/hi";
+import { RiSendPlaneFill as SendIcon } from "react-icons/ri";
 
 import { Context } from "../Data/Context";
 
 const ChatPage = () => {
+  const toast = useToast();
   const { userDetails } = useContext(Context);
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  }: any = useSpeechRecognition();
   const [showLogOut, setShowLogOut] = useState(false);
   const [showOptions, setShowOptions] = useState(false);
   const [interviewType, setInterviewType] = useState("");
+  const [inpVal, setInpVal] = useState("");
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  // If Browser Doesn't Support this.
+  if (!browserSupportsSpeechRecognition) {
+    return (
+      <Text bg="bgB" fontSize={["24px"]} textAlign="center" color="textColor">
+        Your browser does not support this.
+      </Text>
+    );
+  }
+
+  // Transcript UseEffect
+  useEffect(() => {
+    setInpVal(transcript);
+  }, [transcript]);
+
+  // handle Mic Click
+  const handleMicClick = () => {
+    if (!listening) {
+      toast({
+        title: `Microphone : ON`,
+        status: "success",
+        position: "top",
+        duration: 1000,
+      });
+      startListening();
+    } else if (listening) {
+      toast({
+        title: `Microphone : OFF`,
+        status: "info",
+        position: "top",
+        duration: 1000,
+      });
+      SpeechRecognition.stopListening();
+      resetTranscript();
+    }
+  };
+
+  // Submit Function
+  const handleSubmit = () => {
+    if (inpVal == "") {
+      return toast({
+        title: `Empty Response !`,
+        position: "top",
+        status: "error",
+        duration: 2000,
+      });
+    }
+    setInpVal("");
+  };
 
   return (
     <Box css={css.Outer}>
@@ -27,7 +93,7 @@ const ChatPage = () => {
       <Box bg="bgB" css={css.LeftCont}>
         {/* UserName */}
         <Box bg="bgA" color="textColor" css={css.UserNameBox}>
-          <Text>{`Hi!  ${userDetails}`}</Text>
+          <Text>{`Hi! ${userDetails}`}</Text>
           <Image
             onClick={() => setShowLogOut((prev) => !prev)}
             _hover={{ color: "hovertext" }}
@@ -83,8 +149,36 @@ const ChatPage = () => {
       {/************************  Right Side *************************/}
       <Box css={css.RightCont}>
         {/* <div id="testing"></div> */}
-
-        <Box css={css.ParentContRight}></Box>
+        <Box css={css.ParentContRight}>
+          {/* Input Component */}
+          <Box bg="bgB" css={css.InpContCss}>
+            <InputGroup>
+              <InputLeftElement
+                onClick={handleMicClick}
+                color={listening ? "greenShade" : "textcolor"}
+                css={css.InputIconsCss}
+              >
+                <MicIcon />
+              </InputLeftElement>
+              <Input
+                color="hovertext"
+                _placeholder={{ color: "hovertext" }}
+                focusBorderColor="transparent"
+                value={inpVal}
+                onChange={(e) => setInpVal(e.target.value)}
+                placeholder="Your Answer"
+              />
+              <InputRightElement
+                onClick={handleSubmit}
+                color="hovertext"
+                _hover={{ color: "greenShade" }}
+                css={css.InputIconsCss}
+              >
+                <SendIcon />
+              </InputRightElement>
+            </InputGroup>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
