eeedc9224c528a85c12fbef97859530c6e365a47
Anshul Kushwah <kushwahasg4450@gmail.com>
2023-09-01 18:18:38 +0530

    Auth

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import { Box } from "@chakra-ui/react";
+import { SignUp } from "./Pages/SignUp";
+import { Login } from "./Pages/Login";
 
 function App() {
-  return <Box className="bg-red-500">Hello</Box>;
+  return <Box className="bg-red-5000">
+    <Login/>
+    {/* <SignUp/> */}
+  </Box>;
 }
 
 export default App;
