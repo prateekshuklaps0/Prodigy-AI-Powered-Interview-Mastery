b52584b79459f991385a3e0c3b863ea9235840fa
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-09-02 11:35:44 +0530

    Update

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -12,8 +12,7 @@ import { SignUp } from "./Pages/SignUp";
 import { Login } from "./Pages/Login";
 
 function App() {
-  const { token } = useContext(Context);
-  const [showSignUpBox, setShowSignUpBox] = useState(false);
+  const { token, showSignUpBox, setShowSignUpBox } = useContext(Context);
 
   const ParticleInit = useCallback((main: any) => {
     loadFull(main);
@@ -23,9 +22,9 @@ function App() {
     <Box id="App">
       <Particles options={ParticleOptions} init={ParticleInit} />
 
-      {!token && <ChatPage />}
+      {token && <ChatPage />}
 
-      {token && (
+      {!token && (
         <Box>
           <Box
             m="auto"
