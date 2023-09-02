722c471aaf32abb8684066f9bd126d47465b68aa
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-09-02 19:49:12 +0530

    Added Images

--- Front-End/src/Pages/ChatPage.tsx
+++ Front-End/src/Pages/ChatPage.tsx
@@ -99,35 +99,34 @@ export const ChatPage = () => {
   const handleChatBtn = () => {
     setLoading(true);
     fetch(`${API_Url}/interview/questions`, requestOptions)
-  .then((response) => {
-    // console.log(response)
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data)
-    let firstQuestion = {
-      content: data.question.choices[0].message.content,
-      type: "Response",
-    };
-    setResponseContent((prev:any) => [...prev, firstQuestion]);
-    setQuestion(firstQuestion.content);
-    setBtnText("Next Question");
-    setLoading(false);
-  })
-  .catch((error) => {
-    console.error("There was a problem with the fetch operation:", error);
-    toast({
-      title: "Something Went Wrong",
-      status: "error",
-      position: "top-right",
-      duration: 2000,
-    });
-    setLoading(false);
-  });
-
+      .then((response) => {
+        // console.log(response)
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        let firstQuestion = {
+          content: data.question.choices[0].message.content,
+          type: "Response",
+        };
+        setResponseContent((prev: any) => [...prev, firstQuestion]);
+        setQuestion(firstQuestion.content);
+        setBtnText("Next Question");
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+        toast({
+          title: "Something Went Wrong",
+          status: "error",
+          position: "top-right",
+          duration: 2000,
+        });
+        setLoading(false);
+      });
 
     // setLoading(true);
     // axios({
