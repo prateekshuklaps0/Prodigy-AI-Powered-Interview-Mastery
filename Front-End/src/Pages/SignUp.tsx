1e1753e2cdb03ccbc55b3119eede2d1840d8760d
Anshul Kushwah <kushwahasg4450@gmail.com>
2023-09-02 15:21:54 +0530

    log-in-done

--- Front-End/src/Pages/SignUp.tsx
+++ Front-End/src/Pages/SignUp.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
-// import axios from "axios";
+import { useContext } from "react";
+import { Context } from "../Data/Context";
+import { useToast } from '@chakra-ui/react'
+
 
 let initialData = {
   name: "",
@@ -8,7 +11,10 @@ let initialData = {
 };
 
 export const SignUp = () => {
+  const { setShowSignUpBox } = useContext(Context)
   const [inputData, setInputData] = useState(initialData);
+  const toast = useToast()
+
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = e.target;
@@ -23,71 +29,88 @@ export const SignUp = () => {
 
     // axios.post(`http://localhost:5000/user/register`, inputData)
     //   .then((res) => {
-    //     console.log(res.data);
-    //     setInputData(initialData);
+    //     console.log(res.data)
+    //     setInputData(initialData)
     //   })
     //   .catch((error) => {
-    //     console.error("Error:", error);
-    //   });
-    fetch('http://localhost:5000/user/register', {
-      method: 'POST',
+    //     console.error("Error:", error)
+    //   })
+
+    fetch("http://localhost:5000/user/register", {
+      method: "POST",
       headers: {
-          'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(inputData),
-      })
-      .then(response => response.json())
-      .then(data => {
+    })
+      .then((response) => response.json())
+      .then((data) => {
         setInputData(initialData);
-          console.log(data);
+        setShowSignUpBox(true)
+        toast({
+          title: 'Registered Successfully',
+          status: 'success',
+          position:"top",
+          duration: 1000,
+          isClosable: true,
+        })
+        console.log(data);
       })
-      .catch(error => {
-          console.error('Error:', error);
-      });
-
+      .catch((error) => {
+        console.error("Error:", error)
+      })
   }
 
   return (
-    <div className="border-2 max-w-xl m-auto mt-32 h-100 p-4">
-      <h2 className="font-medium text-2xl text-zinc-700">Sign Up</h2>
+    <div 
+      className="  rounded-lg max-w-xl m-auto mt-32 h-100 p-6"
+      style={{boxShadow: "rgba(160, 150, 177, 0.489) 0px 3px 6px, rgba(160, 150, 177, 0.4) 0px 7px 13px -3px, rgba(160, 150, 177, 0.4) 0px -1px 5px inset"}}
+    >
+      <h2 className="font-medium text-3xl text-white">Sign Up</h2>
       <div>
         <form onSubmit={handleSubmit}>
           <br />
-          <i className="fa-solid fa-user text-zinc-500"></i>
+          <i className="fa-solid fa-user text-zinc-100 mr-4"></i>
           <input
             type="text"
             name="name"
             value={inputData.name}
             placeholder="name"
-            className="text-left border-2 outline-none h-9 w-2/4 text-zinc-600"
+            className="text-left border-2 outline-none h-9 w-2/4 text-zinc-800 font-medium rounded"
             onChange={handleChange}
+            required
           />
           <br />
           <br />
-          <i className="fa-solid fa-envelope text-zinc-500"></i>
+          <i className="fa-solid fa-envelope text-zinc-100 mr-4"></i>
           <input
             type="text"
             name="email"
             value={inputData.email}
             placeholder="email"
-            className="text-left border-2 outline-none h-9 w-2/4 text-zinc-600"
+            className="text-left border-2 outline-none h-9 w-2/4 text-zinc-800 font-medium rounded"
             onChange={handleChange}
+            required
           />
           <br />
           <br />
-          <i className="fa-solid fa-lock text-zinc-500"></i>
+          <i className="fa-solid fa-lock text-zinc-100 mr-4"></i>
           <input
             type="password"
             name="password"
             value={inputData.password}
             placeholder="password"
-            className="text-left border-2 outline-none h-9 w-2/4 text-zinc-600"
+            className="text-left border-2 outline-none h-9 w-2/4 text-zinc-800 font-medium rounded"
             onChange={handleChange}
+            required
           />
           <br />
           <br />
-          <button className="border-2 p-2 pl-10 pr-10 text-white font-medium rounded-md bg-indigo-400 hover:bg-indigo-500">
-            Submit
+          <button 
+            className="border-2 p-2 pl-10 pr-10 text-white font-medium rounded-md bg-purple-700  hover:bg-gradient-to-r from-purple-600 to-purple-900  "
+            
+          >
+            Register
           </button>
         </form>
       </div>
